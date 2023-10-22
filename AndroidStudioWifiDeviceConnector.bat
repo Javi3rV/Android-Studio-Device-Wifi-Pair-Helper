0654bd3bf1aeb01e938295ced2fd02253c98c476
Javi3rV <74859199+Javi3rV@users.noreply.github.com>
2023-10-22 11:45:25 +0200

    Removed the connect function since it should auto connect when paired

--- AndroidStudioWifiDeviceConnector.bat
+++ AndroidStudioWifiDeviceConnector.bat
@@ -1,46 +1,24 @@
 @echo off
 echo.
 echo. Let's try to connect your phone to android studio
-pause
-cd /D %LOCALAPPDATA%\Android\sdk\platform-tools
-set /p ans=" Is your phone already paired with %computername%? (Y/N): "
-if "%ans%"=="Y" goto paired
-if "%ans%"=="y" goto paired
-if "%ans%"=="N" goto pair
-if "%ans%"=="n" goto pair
-:pair
+echo.
+echo. Press any key to continue
+pause >nul
+cd /D %LOCALAPPDATA%\Android\sdk\platform-tools >nul
 timeout /t 1 /nobreak >nul
+cls
 echo.
 echo. - In your phone, go to developer options and enable Wireless Debugging 
 echo. - Go into wireless debugging screen, select pair using pairing code (NOT QR)
 echo. - You will see an alert with "<ip>:<port>" values, and a pairing code
 echo. - Then, press any key to continue
 echo.
-pause
-set /p ip=" Enter your <ip> value> "
-set /p port=" Enter your <port> value> "
-echo. pairing...
+pause >nul
+set /p ip=" Enter your <ip> value -> "
+set /p port=" Enter your <port> value -> "
+echo. Trying to connect...
 adb pair %ip%:%port%
-timeout /t 1 /nobreak >nul
-echo.
-echo. Now go to wireless debugging main screen, there is another "<ip>:<port>" 
-echo. with same ip, but different port.
-set /p secondPort=" Enter port value> "
-adb connect %ip%:%secondPort%
-pause
-exit
-
-:paired
-set /p ip=" Enter your <ip> value> "
-set /p port=" Enter your <port> value> "
-echo. connecting...
-adb pair %ip%:%port%
-timeout /t 1 /nobreak >nul
-echo.
-pause
-exit
-
 timeout /t 1 /nobreak >nul
 echo.
 echo. Press any key to exit
-pause
+pause >nul
